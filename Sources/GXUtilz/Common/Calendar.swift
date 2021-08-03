f11b6d624f0b8f968c5e241609d52737e4ea63a4
Anton Agafonov <otlichnonick@yandex.ru>
2021-08-03 16:30:04 +0300

    Задокументировал расширение для Calendar

--- Sources/GXUtilz/Common/Calendar.swift
+++ Sources/GXUtilz/Common/Calendar.swift
@@ -8,6 +8,12 @@
 import Foundation
 
 public extension Calendar {
+    /// Функция возвращает количество дней, месяцев или лет между двумя датами
+    /// - Parameters:
+    ///   - startDate: Дата начала периода
+    ///   - endDate: Дата окончания периода
+    ///   - components: Компонент даты: день, месяц или год
+    /// - Returns: Опциональное количество дней, месяцев или лет
     func periodBetweenDays(_ startDate: Date, _ endDate: Date, with components: Set<Calendar.Component>) -> Int? {
         let startDate = startOfDay(for: startDate)
         let endDate = startOfDay(for: endDate)
