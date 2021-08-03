ff612e5a968fb3d533cdf2f73352b4782c9a5d08
Danil Lomaev <danlomaev@yandex.ru>
2021-08-03 13:22:37 +0300

    Обновил extension для View, добавил документацию

--- Sources/GXUtilz/View/HideKeyboard.swift
+++ Sources/GXUtilz/View/HideKeyboard.swift
@@ -9,7 +9,8 @@ import Foundation
 import SwiftUI
 #if canImport(UIKit)
 import UIKit
-    extension View {
+public extension View {
+    /// Удобная обертка, которую можно использовать во всех SwiftUI View для скрытия клавиатуры программно
         func hideKeyboard() {
             UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
         }
