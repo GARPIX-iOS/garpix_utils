cb789b8a399e03394b7e97b3051f73a77b8ab44b
Anton Vlezko <vlezko.anton@yandex.ru>
2021-08-05 16:25:29 +0300

    Добавил init

--- Sources/GXUtilz/Structs/StringInputType.swift
+++ Sources/GXUtilz/Structs/StringInputType.swift
@@ -9,7 +9,7 @@ import Foundation
 
 /// Этот протокол позволяет задать пользователю ограничения на содержимое строки, а также через метод calculateTextReplacing получить строку содержимое которой не будет удаляться из текстфилда при форматировании
 public protocol StringInputTypeProtocol {
-    var formatLanguage: FormatWithLanguage { get set }
+    var formatLanguage: FormatWithLanguage? { get set }
     var containsText: Bool { get set }
     var containsNumbers: Bool { get set }
     var containsSpecialSymbols: Bool { get set }
@@ -22,7 +22,7 @@ extension StringInputTypeProtocol {
     /// - Returns: Строку необходимую для замены текст неподходящего под форматирование на ""
     func calculateTextReplacing() -> String {
         var text = ""
-        let textReplacing = formatLanguage.textReplacing
+        let textReplacing = formatLanguage?.textReplacing ?? FormatWithLanguage.eng.textReplacing
         let numbersReplacing = "[^0-9]"
         let specialSymbols = " !\"#$%&'()*+,-./:;<=>?@\\[\\\\\\]^_`{|}~].{8,}$"
         
@@ -42,9 +42,23 @@ extension StringInputTypeProtocol {
     }
 }
 
+/// Структура подписанная под протокол StringInputTypeProtocol упрощающая взаимодействие клиента с функциями форматирования текста
 public struct StringInputType: StringInputTypeProtocol {
-    public var formatLanguage: FormatWithLanguage
+    public var formatLanguage: FormatWithLanguage?
     public var containsText: Bool
     public var containsNumbers: Bool
     public var containsSpecialSymbols: Bool
+    
+    /// Если не выбрать параметр форматирования языка то в таком случае вы получите init с английским языком
+    public init(
+        formatLanguage: FormatWithLanguage? = nil,
+        containsText: Bool,
+        containsNumbers: Bool,
+        containsSpecialSymbols: Bool
+    ) {
+        self.formatLanguage = formatLanguage
+        self.containsText = containsText
+        self.containsNumbers = containsNumbers
+        self.containsSpecialSymbols = containsSpecialSymbols
+    }
 }
