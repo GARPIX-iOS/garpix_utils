59b17501af49568fe3801848618e3ede5298e7cf
WeDontNeedTo <danlomaev@yandex.ru>
2021-11-12 18:50:18 +0300

    adde opacity

---
+++ Sources/GXUtilz/View/Conditions.swift
@@ -0,0 +1,53 @@
+//
+//  File.swift
+//  
+//
+//  Created by Danil on 12.11.2021.
+//
+
+import Foundation
+import SwiftUI
+
+struct OpacityIf: ViewModifier {
+    @Binding var condition: Bool
+    @Binding var value: Double
+
+
+    func body(content: Content) -> some View {
+        content
+            .opacity(condition ? value : 1.0)
+    }
+}
+
+extension View {
+    /// Если условие правдивое, то на View будет действовать opacity с определенным значением
+    /// - Parameters:
+    ///   - condition: условие
+    ///   - value: значение прозрачности
+    /// - Returns: новая View
+    func opacityIf(condition: Binding<Bool>, value: : Binding<Bool>) -> some View {
+        modifier(OpacityIf(condition: condition, value: value))
+    }
+}
+
+struct OpacityIfNot: ViewModifier {
+    @Binding var condition: Bool
+    @Binding var value: Double
+
+
+    func body(content: Content) -> some View {
+        content
+            .opacity(condition ? 1.0 : value)
+    }
+}
+
+extension View {
+    /// Если условие ложное, то на View будет действовать opacity с определенным значением
+    /// - Parameters:
+    ///   - condition: условие
+    ///   - value: значение прозрачности
+    /// - Returns: новая View
+    func opacityIfNot(condition: Binding<Bool>, value: : Binding<Bool>) -> some View {
+        modifier(OpacityIfNot(condition: condition, value: value))
+    }
+}
