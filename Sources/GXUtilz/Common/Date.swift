f94af103a098d37130ca1bfffb5dccc2ef5f7af9
Yunus Ishchanov <61401964+Devishyns@users.noreply.github.com>
2021-12-21 19:01:27 +0300

    Add to utils useful extensions (#15)
    * Создание:
    - расширение которое превращает массив в сериализованную строку
    - расширение которое сжимает изображения и преобразует его в JPEG, уменьшая его размер и качество
    
    * add public to extensions
    
    * https://pm.garpix.com/browse/DEV-5-add to utils useful extensions

--- Sources/GXUtilz/Common/Date.swift
+++ Sources/GXUtilz/Common/Date.swift
@@ -93,3 +93,107 @@ public extension DateFormatter {
         return formatter
     }
 }
+
+public extension Numeric {
+    /**
+     Преобразует `Double&Int` в ₽
+
+     Например:
+     - Входные данные
+     ```
+     var employeeTotal: Double = 10.000
+     var materialCosts: Int = 45.000
+     ```
+     Использование:
+     ```
+     Text(employeeTotal.getPrice())
+     Text(materialCosts.getPrice())
+     ```
+     */
+    func getPrice() -> String {
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        currencyFormatter.maximumFractionDigits = 0
+        currencyFormatter.locale = Locale(identifier: "ru_RU")
+
+        let price = self
+
+        var nsPrice: NSNumber?
+
+        if price is Double {
+            nsPrice = NSNumber(value: price as! Double)
+        }
+
+        if price is Int {
+            nsPrice = NSNumber(value: price as! Int)
+        }
+
+        if let price = nsPrice, let priceString = currencyFormatter.string(from: price) {
+            return priceString
+        }
+        return ""
+    }
+}
+
+public extension Date {
+    /**
+     Функция поможет когда необходимо отображать только год.
+     - Warning: В случае `String` сначала надо будет его преобразовать в `Date` с помощью stringToDate.
+     
+     Например:
+     - Входные данные
+     ```
+     var exampleDate: String = ""
+     ```
+     Использование:
+     ```
+     Text(exampleDate.stringToDate()?.formatDate() ?? "")
+     ```
+    */
+    func formatDateToYear() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.setLocalizedDateFormatFromTemplate("yyyy")
+        return dateFormatter.string(from: self)
+    }
+    
+    
+}
+
+public extension Formatter {
+    static let ruCurrency: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.maximumFractionDigits = 0
+        formatter.locale = .init(identifier: "ru_RU")
+        formatter.numberStyle = .currency
+        return formatter
+    }()
+}
+
+public extension String {
+    var double: Double? {
+        Double(self)
+    }
+    /**
+     Преобразует `String` в ₽
+
+     Например:
+     - Входные данные
+     ```
+     var price: String = "3434.00"
+     ```
+     Использование:
+     ```
+     Text(price.ruCurrencyFormatted)
+     ```
+
+     В результате получаем:
+     ```
+     3 434 ₽
+     ```
+     */
+    var ruCurrencyFormatted: String {
+        Formatter.ruCurrency.string(for: double) ?? Formatter.ruCurrency.string(for: 0) ?? ""
+    }
+}
--- Sources/GXUtilz/Common/String.swift
+++ Sources/GXUtilz/Common/String.swift
@@ -165,3 +165,15 @@ public extension String {
         return true
     }
 }
+
+// MARK: - Check URL valid
+
+public extension String {
+    /// Функция для проверки валидности URL-адреса
+    func isValidURL() -> Bool {
+        guard let url = URL(string: self), UIApplication.shared.canOpenURL(url) else { return false }
+        let regEx = "((https|http)://)((\\w|-)+)(([.]|[/])((\\w|-)+))+"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", argumentArray: [regEx])
+        return predicate.evaluate(with: self)
+    }
+}
